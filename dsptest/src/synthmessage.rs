fe0ad6479fae6a60b3e3c5f259f6129de7f8f853
Andy Best <andybest.net@gmail.com>
2021-05-08 18:19:25 +0100

    Add basic MIDI keyboard support

--- dsptest/src/synthmessage.rs
+++ dsptest/src/synthmessage.rs
@@ -1,5 +1,5 @@
 #[derive(Debug)]
 pub enum SynthMessage {
-    Note,
-    Foo
+    NoteOn(u8),
+    NoteOff(u8)
 }
--- dsptest/src/synthui.rs
+++ dsptest/src/synthui.rs
@@ -3,13 +3,14 @@ use iced::{
     Text,
 };
 use iced_native::{window, Event};
-use std::sync::mpsc;
+use std::{collections::HashMap, sync::mpsc};
 
 use crate::synthmessage::SynthMessage;
 
 pub struct SynthUI {
     should_exit: bool,
     tx: mpsc::Sender<SynthMessage>,
+    key_map: HashMap<u8, bool>,
 }
 
 #[derive(Default)]
@@ -33,6 +34,7 @@ impl Application for SynthUI {
             SynthUI {
                 should_exit: false,
                 tx: flags.tx.expect("No TX"),
+                key_map: HashMap::new(),
             },
             Command::none(),
         )
@@ -51,6 +53,8 @@ impl Application for SynthUI {
     }
 
     fn update(&mut self, message: Message, _clipboard: &mut Clipboard) -> Command<Message> {
+        let mut synth_message: Option<SynthMessage> = None;
+
         match message {
             Message::EventOccurred(event) => {
                 if let Event::Window(window::Event::CloseRequested) = event {
@@ -60,13 +64,13 @@ impl Application for SynthUI {
                     modifiers: _,
                 }) = event
                 {
-                    println!("Key pressed: {}", key_code as u32);
+                    synth_message = self.update_midi_keys(key_code, true);
                 } else if let Event::Keyboard(keyboard::Event::KeyReleased {
                     key_code,
                     modifiers: _,
                 }) = event
                 {
-                    println!("Key released: {}", key_code as u32);
+                    synth_message = self.update_midi_keys(key_code, false);
                 }
             }
             Message::Exit => {
@@ -74,8 +78,9 @@ impl Application for SynthUI {
             }
         };
 
-        self.tx.send(SynthMessage::Note).expect("Unable to send");
-        self.tx.send(SynthMessage::Foo).expect("Unable to send");
+        if let Some(msg) = synth_message {
+            self.tx.send(msg);
+        }
 
         Command::none()
     }
@@ -85,5 +90,58 @@ impl Application for SynthUI {
             .padding(20)
             .push(Text::new("Hello, world!"))
             .into()
+    }
+}
+
+impl SynthUI {
+    fn key_to_note(code: keyboard::KeyCode) -> Option<u8> {
+        use keyboard::KeyCode as K;
+        match code {
+            K::A => Some(60),
+            K::W => Some(61),
+            K::S => Some(62),
+            K::E => Some(63),
+            K::D => Some(64),
+            K::F => Some(65),
+            K::T => Some(66),
+            K::G => Some(67),
+            K::Y => Some(68),
+            K::H => Some(69),
+            K::U => Some(70),
+            K::J => Some(80),
+            K::K => Some(81),
+            K::O => Some(82),
+            K::L => Some(83),
+            K::P => Some(84),
+            K::Semicolon => Some(85),
+            K::Apostrophe => Some(86),
+            _ => None,
+        }
+    }
+
+    fn update_midi_keys(&mut self, code: keyboard::KeyCode, pressed: bool) -> Option<SynthMessage> {
+        if let Some(note) = SynthUI::key_to_note(code) {
+            if let Some(was_pressed) = self.key_map.get(&note) {
+                if *was_pressed != pressed {
+                    self.key_map.insert(note, pressed);
+
+                    return Some(if pressed {
+                        SynthMessage::NoteOn(note)
+                    } else {
+                        SynthMessage::NoteOff(note)
+                    });
+                }
+            } else {
+                self.key_map.insert(note, pressed);
+
+                return Some(if pressed {
+                    SynthMessage::NoteOn(note)
+                } else {
+                    SynthMessage::NoteOff(note)
+                });
+            }
+        }
+        
+        None
     }
 }
